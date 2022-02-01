33683e13ae6bd5619062569ec0aaf960788cf28f
Ryan <rnward0@gmail.com>
2022-02-01 14:16:55 -0700

    task 1 url builder in typescript

---
+++ assignment1/task1Typescript/index.ts
@@ -0,0 +1,31 @@
+{
+  function add(x: number): (y: number) => number {
+    return function (y: number): number {
+      return x + y;
+    };
+  }
+
+  const res1: Function = add(1);
+  const res2: number = res1(10);
+
+  const res3: number = add(10)(10);
+
+  console.log(res3);
+}
+
+{
+  function urlBuilder(
+    protocol: string
+  ): (domainName: string) => (extension: string) => string {
+    return function (domainName: string): (extension: string) => string {
+      return function (extension: string): string {
+        return protocol + "://" + domainName + "." + extension;
+      };
+    };
+  }
+
+  const protocol = urlBuilder("https");
+  const domainName = protocol("heyauto");
+  const full = domainName("com");
+  console.log(full);
+}
