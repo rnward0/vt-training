92a13fab746a916b949a1e0d71625518886071ea
Ryan <rnward0@gmail.com>
2022-02-02 14:12:44 -0700

    added example to task 3

--- assignment1/task3/index.ts
+++ assignment1/task3/index.ts
@@ -1,3 +1,5 @@
+import { pipe } from "ramda";
+
 type UnaryFunction<A, B> = (x: A) => B;
 type BinaryFunction<A, B, C> = (y: A, z: B) => C;
 
@@ -8,7 +10,20 @@ const f = <A, B, C>(g: BinaryFunction<A, B, C>, a: A): UnaryFunction<B, C> => {
   };
 };
 
-//Step 2: partial
+function multiplyNumbers(x: number, y: number): number {
+  return x * y;
+}
+
+function printANumber(result: number): void {
+  console.log(result);
+}
+
+//Step 2: Function name is partial
 
 //Step 3: Demonstrate use
 console.log(f(Math.pow, 2)(2));
+
+console.log(f(multiplyNumbers, 2)(4));
+
+const result = pipe(f(multiplyNumbers, 4), printANumber);
+result(4);
