7f63454fe16a68ffe2148a59330866ee511e4551
Ryan <rnward0@gmail.com>
2022-02-07 16:45:43 -0700

    task 1, task 2 changes

--- assignment2/task1/index.ts
+++ assignment2/task1/index.ts
@@ -1,26 +1,24 @@
 //f: A -> [A] -> [A]
-function replace<A>(a: A, arr: [A]): [A] {
-  arr[0] = a;
-  return arr;
-}
+const filter = <A>(a: A, arr: A[]): A[] => {
+  return arr.filter((item: A) => item !== a);
+};
 
 //f: Number -> Number -> [A] -> [A]
-function f<A>(x: Number, y: Number, val: [A]): [A] {
+const f = <A>(x: Number, y: Number, val: A[]): A[] => {
   return val;
-}
+};
 
 //f: [String] -> {String: any} -> {String: any}
-function g(val: [String], obj: { String: any }): { String: any } {
+const g = (val: [String], obj: { String: any }): { String: any } => {
   return { String: 1 };
-}
+};
 
 //f: [A] → [B] → [[A,B]]
-function combine<A, B>(val: [A], val2: [B]): [[A, B]] {
+const combine = <A, B>(val: A[], val2: B[]): [[A, B]] => {
   return [[val[0], val2[0]]];
-}
+};
 
 //Testing
-console.log(replace(1, [2]));
-console.log(f(1, 2, ["something"]));
+console.log(filter(1, [2, 2, 3, 4, 1]));
 
 console.log(combine([1], ["hello"]));
--- assignment2/task2/index.ts
+++ assignment2/task2/index.ts
@@ -20,3 +20,44 @@ interface Comment extends Entity {
 type ApiResponse<T extends Entity> =
   | { status: "success"; data: T[] }
   | { status: "error"; error: string };
+
+type email = {
+  address: string;
+};
+
+const mockComments = [
+  {
+    __tag: "comment",
+    postId: 0,
+    name: "mock1",
+    email: "mock1@mock.mock",
+    body: "mock data 1",
+  },
+];
+
+const mockPosts: Post[] = [
+  {
+    id: "0",
+    __tag: "post",
+    userId: 0,
+    title: "my post",
+    body: "post body",
+  },
+  {
+    id: "1",
+    __tag: "post",
+    userId: 1,
+    title: "my post 2",
+    body: "post body 2",
+  },
+];
+
+//Del. 1
+const fetchMockData = (variant: string) => {};
+
+//Del. 2
+const fetchComments = () => {};
+
+const fetchPosts = () => {};
+
+//Del. 3
