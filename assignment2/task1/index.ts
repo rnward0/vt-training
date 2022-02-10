4c201e1781cb3119e4ff48f58b207a77521ff62b
Ryan <rnward0@gmail.com>
2022-02-10 16:57:26 -0700

    task 1, task 2 changes

--- assignment2/task1/index.ts
+++ assignment2/task1/index.ts
@@ -3,17 +3,24 @@ const filter = <A>(a: A, arr: A[]): A[] => {
   return arr.filter((item: A) => item !== a);
 };
 
+const concat = <A>(a: A, arr: A[]): A[] => {
+  return arr.concat(a);
+};
+
 //f: Number -> Number -> [A] -> [A]
 const slice = <A>(start: number, end: number, val: A[]): A[] => {
   return val.slice(start, end);
 };
 
 //f: [String] -> {String: any} -> {String: any}
-const extract = (val: string[], obj: Record<string, any>): Record<string, any> => {
+const extract = (
+  val: string[],
+  obj: Record<string, any>
+): Record<string, any> => {
   const newObj: Record<string, any> = {};
   val.forEach((item: string) => {
-    if(obj[item] !== undefined) {
-      newObj[item] = obj[item]
+    if (obj[item] !== undefined) {
+      newObj[item] = obj[item];
     }
   });
   return newObj;
@@ -26,6 +33,7 @@ const combine = <A, B>(val: A[], val2: B[]): [A[], B[]] => {
 
 //Usage
 console.log(filter(1, [2, 2, 3, 4, 1]));
+console.log(concat(1, [5, 4, 3, 2]));
 console.log(slice(1, 5, ["a", "b", "c", "d", "e", "f"]));
-console.log(extract(["a", "b", "c", "d"], { "b": 1, "c": 2 }));
+console.log(extract(["a", "b", "c", "d"], { b: 1, c: 2 }));
 console.log(combine([1, 2, 3, 4], ["a", "b", "c", "d"]));
--- assignment2/task2/src/App.tsx
+++ assignment2/task2/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { right, left, Either } from "fp-ts/lib/Either";
 
 interface Entity {
   id: string;
@@ -24,24 +25,36 @@ type ApiResponse<T extends Entity> =
   | { status: "error"; error: string };
 
 const App: React.FC = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<ApiResponse<Comment | Post>>({
+    status: "error",
+    error: "data not fetched yet!",
+  });
 
   //Del. 1
   const fetchMockData = (variant: "posts" | "comments") => {
     return (id?: number) => {
       if (id !== undefined) {
-        return fetch(
-          `https://jsonplaceholder.typicode.com/${variant}/?postId=${id}`
-        )
-          .then((res) => res.json())
-          .then((res) => {
-            return { status: "success", data: res };
-          });
+        if (variant === "posts") {
+          return fetch(`https://jsonplaceholder.typicode.com/${variant}/${id}`)
+            .then((res) => res.json())
+            .then((res) => {
+              return match(right(res));
+            });
+        } else {
+          return fetch(
+            `https://jsonplaceholder.typicode.com/${variant}/?postId=${id}`
+          )
+            .then((res) => res.json())
+            .then((res) => {
+              return match(right(res));
+            });
+        }
       } else {
+        console.log("id not defined");
         return fetch(`https://jsonplaceholder.typicode.com/${variant}`)
           .then((res) => res.json())
           .then((res) => {
-            return { status: "success", data: res };
+            return match(right(res));
           });
       }
     };
@@ -56,33 +69,51 @@ const App: React.FC = () => {
     return fetchMockData("posts");
   };
 
-  const match = (obj: ApiResponse<Comment | Post>) => {
-    switch(obj.status) {
-      case("success"):
-        return "success";
-      case("error"):
-        return "error";
+  const match = (
+    val: Either<string, Comment[] | Post[]>
+  ): ApiResponse<Comment | Post> => {
+    switch (val._tag) {
+      case "Left":
+        const objL: ApiResponse<Comment | Post> = {
+          status: "error",
+          error: val.left,
+        };
+        return objL;
+      case "Right":
+        const objR: ApiResponse<Comment | Post> = {
+          status: "success",
+          data: val.right,
+        };
+        return objR;
     }
   };
 
-  const obtainData = () => {
-    
-  }
+  const obtainData = () => {};
 
   useEffect(() => {
     fetchComments()()
       .then((res) => {
-        console.log(res)
-        setData(res.data)
+        setData(res);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        const errData = match(left(err));
+        setData(errData);
+      });
   }, []);
 
   return (
     <div>
-      {data.map((item: any, index: number) => {
-        return <div key={index}>{item.name}</div>;
-      })}
+      {data.status === "success" ? (
+        data.data.map((item: any, index: number) => {
+          return (
+            <ul key={index}>
+              <div>{item.name}</div>
+            </ul>
+          );
+        })
+      ) : (
+        <div>{data.error}</div>
+      )}
     </div>
   );
 };
