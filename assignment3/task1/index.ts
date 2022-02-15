5277f10d93274f630472cdd44c71b7df53345ccd
Ryan <rnward0@gmail.com>
2022-02-15 16:55:55 -0700

    task 1, task 2 changes

--- assignment3/task1/index.ts
+++ assignment3/task1/index.ts
@@ -1,105 +1,166 @@
-//DEL. 1
-type NotStarted = { __tag: "Not Started" };
-type Progress = { __tag: "Progress"; val: number };
-type Finished<T> = { __tag: "Finished"; val: T };
+{
+  //DEL. 1
+  type NotStarted = { __tag: "Not Started" };
+  type Progress = { __tag: "Progress"; val: number };
+  type Finished<T> = { __tag: "Finished"; val: T };
 
-//DEL. 2
-//Discriminated union type
-type ProgressOption<T> = NotStarted | Progress | Finished<T>;
+  //DEL. 2
+  //Discriminated union type
+  type ProgressOption<T> = NotStarted | Progress | Finished<T>;
 
-//Constructors
-const notStarted = <T>(): ProgressOption<T> => {
-  return { __tag: "Not Started" };
-};
-const progress = <T>(): ProgressOption<T> => {
-  return { __tag: "Progress", val: 0 };
-};
-const finished = <T>(v: T): ProgressOption<T> => {
-  return { __tag: "Finished", val: v };
-};
+  //Constructors
+  const notStarted = <T>(): ProgressOption<T> => {
+    return { __tag: "Not Started" };
+  };
+  const progress = <T>(): ProgressOption<T> => {
+    return { __tag: "Progress", val: 0 };
+  };
+  const finished = <T>(v: T): ProgressOption<T> => {
+    return { __tag: "Finished", val: v };
+  };
 
-//Typeguards
-const isNotStarted = <T>(o: ProgressOption<T>): o is NotStarted =>
-  o.__tag === "Not Started";
-const isProgressing = <T>(o: ProgressOption<T>): o is Progress =>
-  o.__tag === "Progress";
-const isFinished = <T>(o: ProgressOption<T>): o is Finished<T> =>
-  o.__tag === "Finished";
+  //Typeguards
+  const isNotStarted = <T>(o: ProgressOption<T>): o is NotStarted =>
+    o.__tag === "Not Started";
+  const isProgressing = <T>(o: ProgressOption<T>): o is Progress =>
+    o.__tag === "Progress";
+  const isFinished = <T>(o: ProgressOption<T>): o is Finished<T> =>
+    o.__tag === "Finished";
 
-//Helper methods
-const getProgress = <T>(o: ProgressOption<T>): number => {
-  if (isProgressing(o)) {
-    return o.val;
-  }
-  return 0;
-};
+  //Helper methods
+  const getProgress = <T>(o: ProgressOption<T>): number => {
+    if (isProgressing(o)) {
+      return o.val;
+    }
+    return 0;
+  };
 
-const getFinishedVal = <T>(o: ProgressOption<T>): T | string => {
-  if (isFinished(o)) {
-    return o.val;
-  }
-  return "Not finished!";
-};
+  const getFinishedVal = <T>(o: ProgressOption<T>): T | string => {
+    if (isFinished(o)) {
+      return o.val;
+    }
+    return "Not finished!";
+  };
 
-//Del. 3 and extention 2
-const match = <T, G>(
-  val: ProgressOption<T>,
-  a: (t: T) => G,
-  b: (t: number) => G,
-  c: () => G
-): G => {
-  if (isNotStarted(val)) {
-    return c();
-  } else if (isProgressing(val)) {
-    return b(val.val);
-  } else {
-    return a(val.val);
-  }
-};
+  //Del. 3 and extension 2
+  const match = <T, G>(
+    val: ProgressOption<T>,
+    a: (t: T) => G,
+    b: (t: number) => G,
+    c: () => G
+  ): G => {
+    if (isNotStarted(val)) {
+      return c();
+    } else if (isProgressing(val)) {
+      return b(val.val);
+    } else {
+      return a(val.val);
+    }
+  };
 
-//Extension 1
-const map = <T, G>(
-  val: ProgressOption<T>,
-  a: (t: T) => G
-): ProgressOption<G> => {
-  if (isNotStarted(val)) {
-    return notStarted();
-  } else if (isProgressing(val)) {
-    return progress();
-  } else {
-    return finished(a(val.val));
-  }
-};
+  const fold = <T>(opt: ProgressOption<T>, f: () => T): number | T => {
+    if (isNotStarted(opt)) {
+      return f();
+    } else {
+      return opt.val;
+    }
+  };
+
+  //Extension 1
+  const map = <T, G>(
+    val: ProgressOption<T>,
+    a: (t: T) => G
+  ): ProgressOption<G> => {
+    if (isNotStarted(val)) {
+      return notStarted();
+    } else if (isProgressing(val)) {
+      return progress();
+    } else {
+      return finished(a(val.val));
+    }
+  };
+}
 
 //*************Remote Data Implementation ******************/
+{
+  type Initialized = { __tag: "Initialized" };
+  type Pending = { __tag: "Pending" };
+  type Failure<E> = { __tag: "Failure"; e: E };
+  type Success<D> = { __tag: "Success"; data: D };
 
-type Initialized = { __tag: "Initialized" };
-type Pending = { __tag: "Pending" };
-type Failure<E> = { __tag: "Failure"; e: E };
-type Success<D> = { __tag: "Success"; data: D };
+  type RemoteData<E, D> = Initialized | Pending | Failure<E> | Success<D>;
 
-type RemoteData<E, D> = Initialized | Pending | Failure<E> | Success<D>;
+  const initialize = <E, D>(): RemoteData<E, D> => {
+    return { __tag: "Initialized" };
+  };
+  const pending = <E, D>(): RemoteData<E, D> => {
+    return { __tag: "Pending" };
+  };
+  const failure = <E, D>(e: E): RemoteData<E, D> => {
+    return { __tag: "Failure", e: e };
+  };
+  const success = <E, D>(data: D): RemoteData<E, D> => {
+    return { __tag: "Success", data: data };
+  };
 
-const initialize = <E, D>(): RemoteData<E, D> => {
-  return { __tag: "Initialized" };
-};
-const pending = <E, D>(): RemoteData<E, D> => {
-  return { __tag: "Pending" };
-};
-const failure = <E, D>(e: E): RemoteData<E, D> => {
-  return { __tag: "Failure", e: e };
-};
-const success = <E, D>(data: D): RemoteData<E, D> => {
-  return { __tag: "Success", data: data };
-};
+  const isInitialized = <E, D>(val: RemoteData<E, D>): val is Initialized =>
+    val.__tag === "Initialized";
+  const isPending = <E, D>(val: RemoteData<E, D>): val is Pending =>
+    val.__tag === "Pending";
+  const isFailure = <E, D>(val: RemoteData<E, D>): val is Failure<E> =>
+    val.__tag === "Failure";
+  const isSuccess = <E, D>(val: RemoteData<E, D>): val is Success<D> =>
+    val.__tag === "Success";
 
-const isInitialized = <E, D>(val: RemoteData<E, D>): val is Initialized =>
-  val.__tag === "Initialized";
-const isPending = <E, D>(val: RemoteData<E, D>): val is Pending =>
-  val.__tag === "Pending";
-const isFailure = <E, D>(val: RemoteData<E, D>): val is Failure<E> =>
-  val.__tag === "Failure";
-const isSuccess = <E, D>(val: RemoteData<E, D>): val is Success<D> =>
-  val.__tag === "Success";
+  //match function implementation
+  interface Match<T, E, D> {
+    Initialize: () => T;
+    Pending: () => T;
+    Failure: (err: E) => T;
+    Success: (data: D) => T;
+  }
 
-//match function implementation
+  const remoteMatch = <T, E, D>(
+    data: RemoteData<E, D>,
+    matcher: Match<T, E, D>
+  ): T => {
+    if (isInitialized(data)) {
+      return matcher.Initialize();
+    } else if (isPending(data)) {
+      return matcher.Pending();
+    } else if (isFailure(data)) {
+      return matcher.Failure(data.e);
+    } else {
+      return matcher.Success(data.data);
+    }
+  };
+
+  const print = (data: any) =>
+    remoteMatch<number, string, number>(data, {
+      Initialize: () => {
+        console.log("intialized");
+        return 0;
+      },
+      Pending: () => {
+        console.log("pending");
+        return 0;
+      },
+      Failure: (e: string) => {
+        console.log("not available - " + e);
+        return 0;
+      },
+      Success: (data: number) => {
+        console.log("successful! - " + data);
+        return data;
+      },
+    });
+
+  let data: RemoteData<string, number> = initialize();
+  print(data);
+  data = pending();
+  print(data);
+  data = failure("Data not found!");
+  print(data);
+  data = success(20);
+  print(data);
+}
--- assignment3/task2/index.ts
+++ assignment3/task2/index.ts
@@ -1,5 +1,69 @@
-import { some } from "ts-option";
+import { Option, some, option, none } from "ts-option";
+import _ from "lodash";
 
-const something = some(25);
+const numOption: Option<number> = option(10);
+const stringOption: Option<string> = some("something");
+const nothing: Option<never> = none;
 
-console.log(something);
+interface SomeOrNone<A, B> {
+  some: (val: A) => B;
+  none: () => B;
+}
+
+//My implementation of match and fold using ts-option helper methods
+const myMatch = <A, B>(opt: Option<A>, son: SomeOrNone<A, B>): B => {
+  if (opt.isEmpty) {
+    return son.none();
+  } else {
+    return son.some(opt.get);
+  }
+};
+
+const myFold = <A>(opt: Option<A>, f: () => A): ((g: (val: A) => A) => A) => {
+  if (opt.isEmpty) {
+    return f;
+  } else {
+    return (g: (val: A) => A): A => {
+      return g(opt.get);
+    };
+  }
+};
+
+//Usage of ts-option and my match
+console.log(
+  "Original match: " +
+    numOption.match({
+      some: (val: number) => val,
+      none: () => -1,
+    })
+);
+
+console.log(
+  "My match: " +
+    myMatch<number, number>(numOption, {
+      some: (val: number) => val,
+      none: () => -1,
+    })
+);
+
+//Usage of ts-option and my fold
+console.log(
+  "Original fold: " + numOption.fold(() => -1)((val: number) => val * 2)
+);
+
+console.log("My fold " + myFold(numOption, () => -1)((val: number) => val * 2));
+
+//Mapping using Option
+const numArrayOption: Option<Array<number>> = some([1, 2, 3, 4, 5]);
+
+console.log(numArrayOption.get.map((num) => num * 2));
+
+const stringArrayOption: Option<Array<string>> = some([
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+]);
+
+console.log(stringArrayOption.get.map((val) => _.toUpper(val)));
